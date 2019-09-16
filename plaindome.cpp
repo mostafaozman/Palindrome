585a4c614323fc61e3aa929e83f9c8bc3bf077e0
mostafaozman <48702930+mostafaozman@users.noreply.github.com>
2019-09-16 09:58:16 -0700

    Add files via upload

--- plaindome.cpp
+++ plaindome.cpp
@@ -3,6 +3,21 @@
 
 int main() 
 {
+  char input[80]; 
+  char newInput[80]; 
 
+  cout << "This is a palindrome checker, please enter a plaindrome. "
+  cin.get(input, 80)
+  
+    for (int i = 0; i <= 80; i++)
+      {
+	if ( isalunm(input[i]) == false)
+	  {
+	    
+	  }
+	
+      }
+  
+  
   return 0; 
 }
