73bb923b83e72d399481874e06f6cc34dcbf3f4b
mostafaozman <48702930+mostafaozman@users.noreply.github.com>
2019-09-18 10:44:25 -0700

    Add files via upload

--- palindrome.cpp
+++ palindrome.cpp
@@ -5,22 +5,41 @@ using namespace std;
 int main() 
 {
   int z = 0; 
-  char input[80]; 
-  char newInput[z]; 
+  char input[80];  
 
   cout << "This is a palindrome checker, please enter a plaindrome. " << endl;
   cin.get(input, 80); 
-  
-    for (int i = 0; i <= 80; i++)
+  cin.get();
+  for (int i = 0; i <= strlen(input) + 1 ; i++)
       {
 	if (isalnum(input[i]))
-	  {
-	    newInput[z] =  input[i]; 
-	    z++; 
+	  {  
+	    z++;
 	  }
       }
+  char newInput[z];
+  for (int i = 0; i < z + 1; i ++) 
+    {
+    if (isalnum(input[i]))
+      {
+	if (input[i] >= 65 && input[i] <= 90)
+	  {
+	    input[i] += 32; 
+	  }
+      newInput[i] = input[i];
+      cout << newInput[i] << endl;
  
-  cout << newInput << endl; 
+      }
+    }
   
+  char revInput[a]; 
+  for (int i = 0; i <= strlen(newInput); i--)
+    {
+      for (int a = 0; a > strlen(newInput); a++)
+	{
+	  newInput[i] = revInput[a]; 
+	} 
+    } 
+
   return 0; 
 }
