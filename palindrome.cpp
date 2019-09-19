8db189aab529f69cb7a46d5e7b3876da6ea5ccc6
mostafaozman <48702930+mostafaozman@users.noreply.github.com>
2019-09-19 14:06:04 -0700

    Add files via upload

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,3 +1,8 @@
+/*
+By: Mostafa Osman
+This is a plindrome checker, it removes special characters, spaces, and converts to lowercase
+9/19/19
+ */
 #include <iostream>
 #include <cstring> 
 
@@ -5,20 +10,29 @@ using namespace std;
 int main() 
 {
   int z = 0; 
-  char input[80];  
+  int a = 0;
+  int m = 0; 
+  char input[81];  
 
-  cout << "This is a palindrome checker, please enter a plaindrome. " << endl;
-  cin.get(input, 80); 
+  cout << "This is a palindrome checker, please enter a palindrome. " << endl;
+  cin.get(input, 81); 
   cin.get();
-  for (int i = 0; i <= strlen(input) + 1 ; i++)
+  cout <<"Strlen(input) = " << strlen(input)<< endl;
+  for (int i = 0; i <= strlen(input); i++)
       {
 	if (isalnum(input[i]))
 	  {  
 	    z++;
 	  }
       }
+  cout<<"z = "<<z<<endl;
   char newInput[z];
-  for (int i = 0; i < z + 1; i ++) 
+  
+//check for special charcaters and take them out
+  //Make everything lower case
+  // We need a newIndex for the newInput array as we are skipping array elements from the original input array
+  int newIndex = 0;
+  for (int i = 0; i < strlen(input); i ++) 
     {
     if (isalnum(input[i]))
       {
@@ -26,20 +40,43 @@ int main()
 	  {
 	    input[i] += 32; 
 	  }
-      newInput[i] = input[i];
-      cout << newInput[i] << endl;
- 
+	newInput[newIndex] = input[i];
+	cout << "newInput = "<<newInput[newIndex] << endl;
+	newIndex++;
       }
     }
   
-  char revInput[a]; 
-  for (int i = 0; i <= strlen(newInput); i--)
+  //Reverse the input
+  char revInput[z]; 
+  for (int i = z-1; i>= 0; i--)
     {
-      for (int a = 0; a > strlen(newInput); a++)
-	{
-	  newInput[i] = revInput[a]; 
-	} 
+      revInput[i] = newInput[z-i-1]; 
     } 
 
+  //Print the reversed input
+  for ( int b = 0; b < z; b++)
+    {
+      cout <<"revInput ="<<revInput[b] << endl; 
+    }
+
+  for (int b = 0; b < z; b++)
+    {
+      if (revInput[b] == newInput[b])
+	{
+	  m++; 
+	}
+	else
+	  {
+	  }
+    }
+  // Checks for palindrome by comparing number of matching characters 
+      if (m == z)
+	{
+	  cout << "This is a Palindrome!" << endl;
+	}
+      else
+	{
+	  cout << "This is not a Palindrome!";
+	}
   return 0; 
 }
